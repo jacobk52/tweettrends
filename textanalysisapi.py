aad50bed60066e339a550cab425fc8520c7820cc
jacobk52 <jacobkim985@gmail.com>
2021-09-13 22:58:21 -0400

    updated formatting

--- textanalysisapi.py
+++ textanalysisapi.py
@@ -1,8 +1,8 @@
 import os
 from monkeylearn import MonkeyLearn
 
-monkey_learn_api_key=os.environ.get('MONKEY_LEARN_API_KEY')
-monkey_learn_model_id=os.environ.get('MONKEY_LEARN_MODEL_ID')
+monkey_learn_api_key = os.environ.get('MONKEY_LEARN_API_KEY')
+monkey_learn_model_id = os.environ.get('MONKEY_LEARN_MODEL_ID')
 
 def get_sentiment(data):
     m = MonkeyLearn(monkey_learn_api_key)
