a60785f07569823b7a2faa6942539bb91449c21b
jacobk52 <jacobkim985@gmail.com>
2021-09-14 22:17:34 -0400

    formatted code to use a helper, get, function

--- twitterapi.py
+++ twitterapi.py
@@ -7,30 +7,27 @@ def bearer_authorization(a):
     a.headers['Authorization']=f'Bearer {bearer_token}'
     return a
 
+def get(endpoint,params):
+    response = requests.get(endpoint, params=params, auth=bearer_authorization)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
 def get_tweet_info_by_query(query):
     endpoint = 'https://api.twitter.com/2/tweets/search/recent'
     params = {'query':query,'max_results':10}
-    response = requests.get(endpoint, auth=bearer_authorization, params=params)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    response_json = response.json()
+    response_json = get(endpoint, params)
     return [d['text'] for d in response_json['data']]
 
 def get_trends_by_location(location_id):
     endpoint='https://api.twitter.com/1.1/trends/place.json'
     params={'id':location_id}
-    response=requests.get(endpoint,auth=bearer_authorization, params=params)
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    response_json = response.json()
+    response_json = get(endpoint, params)
     return [h['name']for h in response_json[0]['trends']][:3]
 
 def get_available_locations():
     endpoint = 'https://api.twitter.com/1.1/trends/available.json'
-    response = requests.get(endpoint, auth=bearer_authorization, params={})
-    if response.status_code != 200:
-        raise Exception(response.status_code, response.text)
-    response_json = response.json()
+    response_json = get(endpoint,{})
     return [{
         'name':l['name'],
         'location_id':l['woeid'],
@@ -41,3 +38,5 @@ def get_available_locations():
 def get_tweet_info_by_location(location_id):
     trends = get_trends_by_location(location_id)
     return [{trend:[i for i in get_tweet_info_by_query(trend)]} for trend in trends]
+
+print(get_tweet_info_by_query('tammy')[0])
